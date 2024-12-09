16ca834196364c8b940ee0a44f283d804b0fb2bc
sbusiso <sbusisoxaba92@gmail.com>
2024-12-09 13:05:09 +0200

    py

---
+++ Day04/HeadsOrTails.py
@@ -0,0 +1,8 @@
+import random
+
+Heads_Tails = random.ranint(1, 2)
+
+if Heads_Tails == 2:
+    print("Heads")
+else:
+    print("Tails")
---
+++ Day04/random.py
@@ -0,0 +1,7 @@
+import random
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+random_float = random() * 5
+print(random_float)
